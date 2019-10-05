e2c6ba6ac69932cc1d4d5cd5af1df905cb58aece
vithon <capeta.kozo@gmail.com>
2019-10-05 19:35:10 +0900

    2019/10/05

--- haskell/learning/kata.hs
+++ haskell/learning/kata.hs
@@ -192,3 +192,107 @@ instance Enum SixSidedDie where
   fromEnum S4 = 3
   fromEnum S5 = 4
   fromEnum S6 = 5
+
+-- type NameA = (String, String)
+
+-- names :: [Name]
+-- names =
+--   [("Emill", "Cioran"), ("Eugene", "Thacker"), ("Friedrich", "Nietzsche")]
+
+-- instance Ord Name where
+--   compare (f1, l1) (f2, l2) = compare (l1, f1) (l2, f2)
+
+data NameA = NameA (String, String) deriving (Show, Eq)
+instance Ord NameA where
+  compare (NameA (f1, l1)) (NameA (f2, l2)) = compare (l1, f1) (l2, f2)
+
+names :: [NameA]
+names =
+  [ NameA ("Emil", "Cioran")
+  , NameA ("Eugene", "Thacker")
+  , NameA ("Friedrich", "Nietzsche")
+  ]
+
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN alphabetSize c = toEnum rotation
+ where
+  halfAlphabet = alphabetSize `div` 2
+  offset       = fromEnum c + halfAlphabet
+  rotation     = offset `mod` alphabetSize
+
+largestCharNumber :: Int
+largestCharNumber = fromEnum (maxBound :: Char)
+
+rotChar :: Char -> Char
+rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
+  where sizeOfAlphabet = 1 + fromEnum (maxBound :: Char)
+
+fourLetterMessage :: [FourLetterAlphabet]
+fourLetterMessage = [L1, L3, L4, L1, L1, L2]
+
+-- fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+-- fourLetterEncoder vals = map rot4l vals
+--  where
+--   alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+--   rot4l     = rotN alphaSize
+
+data ThreeLetterAlphabet = Alpha | Beta | Kappa deriving (Show, Enum, Bounded)
+
+threeLetterMessage :: [ThreeLetterAlphabet]
+threeLetterMessage = [Alpha, Alpha, Beta, Alpha, Kappa]
+
+-- threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+-- threeLetterEncoder vals = map rot3l vals
+--  where
+--   alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+--   rot3l     = rotN alphaSize
+
+rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNdecoder n c = toEnum rotation
+ where
+  halfN    = n `div` 2
+  offset   = if even n then fromEnum c + halfN else 1 + fromEnum c + halfN
+  rotation = offset `mod` n
+
+-- threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+-- threeLetterDecoder vals = map rot3ldecoder vals
+--  where
+--   alphaSize    = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+--   rot3ldecoder = rotNdecoder alphaSize
+
+rotEncoder :: String -> String
+rotEncoder text = map rotChar text
+ where
+  alphaSize = 1 + fromEnum (maxBound :: Char)
+  rotChar   = rotN alphaSize
+
+rotDecoder :: String -> String
+rotDecoder text = map rotCharDecoder text
+ where
+  alphaSize      = 1 + fromEnum (maxBound :: Char)
+  rotCharDecoder = rotNdecoder alphaSize
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder vals = map rot3l vals
+ where
+  alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+  rot3l     = rotN alphaSize
+
+threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterDecoder vals = map rot3ldecoder vals
+ where
+  alphaSize    = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+  rot3ldecoder = rotNdecoder alphaSize
+
+fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder vals = map rot4l vals
+ where
+  alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+  rot4l     = rotN alphaSize
+
+fourLetterDecoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterDecoder vals = map rot4ldecoder vals
+ where
+  alphaSize    = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+  rot4ldecoder = rotNdecoder alphaSize
