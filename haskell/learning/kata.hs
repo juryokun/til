21008c78be4174032203be963ca590a546f1506b
vithon <capeta.kozo@gmail.com>
2019-10-06 13:31:01 +0900

    2019/10/06

--- haskell/learning/kata.hs
+++ haskell/learning/kata.hs
@@ -296,3 +296,85 @@ fourLetterDecoder vals = map rot4ldecoder vals
  where
   alphaSize    = 1 + fromEnum (maxBound :: FourLetterAlphabet)
   rot4ldecoder = rotNdecoder alphaSize
+
+xorBool :: Bool -> Bool -> Bool
+xorBool v1 v2 = (v1 || v2) && (not (v1 && v2))
+
+xorPair :: (Bool, Bool) -> Bool
+xorPair (v1, v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
+
+type Bits = [Bool]
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = if (remainder == 0)
+  then False : intToBits' nextVal
+  else True : intToBits' nextVal
+ where
+  remainder = n `mod` 2
+  nextVal   = n `div` 2
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+intToBits :: Int -> Bits
+intToBits n = leadingFalses ++ reverseBits
+ where
+  reverseBits   = reverse (intToBits' n)
+  missingBits   = maxBits - (length reverseBits)
+  leadingFalses = take missingBits (cycle [False])
+
+charToBits :: Char -> Bits
+charToBits char = intToBits (fromEnum char)
+
+bitsToInt :: Bits -> Int
+bitsToInt bits = sum (map (\x -> 2 ^ (snd x)) trueLocations)
+ where
+  size          = length bits
+  indices       = [size - 1, size - 2 .. 0]
+  trueLocations = filter (\x -> fst x == True) (zip bits indices)
+
+bitsToChar :: Bits -> Char
+bitsToChar bits = toEnum (bitsToInt bits)
+
+myPad :: String
+myPad = "Shhhhhh"
+
+myPlainText :: String
+myPlainText = "Haskell"
+
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plainText = map (\pair -> (fst pair) `xor` (snd pair))
+                              (zip padBits plaintextBits)
+ where
+  padBits       = map charToBits pad
+  plaintextBits = map charToBits plainText
+
+applyOTP :: String -> String -> String
+applyOTP pad plainText = map bitsToChar bitList
+  where bitList = applyOTP' pad plainText
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myPad
+
+class Cipher a where
+  encode :: a -> String -> String
+  decode :: a -> String -> String
+
+data Rot = Rot
+
+instance Cipher Rot where
+  encode Rot text = rotEncoder text
+  decode Rot text = rotDecoder text
+
+data OneTimePad = OTP String
+
+instance Cipher OneTimePad where
+  encode (OTP pad) text = applyOTP pad text
+  decode (OTP pad) text = applyOTP pad text
+
+myOTP :: OneTimePad
+myOTP = OTP (cycle [minBound .. maxBound])
