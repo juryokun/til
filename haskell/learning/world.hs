6659e6597ae15078b61e3b8ec7555dbf4f08afea
vithon <capeta.kozo@gmail.com>
2019-10-22 23:04:53 +0900

    2019/10/22

--- haskell/learning/world.hs
+++ haskell/learning/world.hs
@@ -1,9 +1,27 @@
-helloPerson :: String -> String
-helloPerson name = "Hello" ++ " " ++ name ++ "!"
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Text                     as T
 
-main :: IO ()
-main = do
-  putStrLn "Hello! What's your name?"
-  name <- getLine
-  let statement = helloPerson name
-  putStrLn statement
+-- helloPerson :: String -> String
+-- helloPerson name = "Hello" ++ " " ++ name ++ "!"
+
+-- main :: IO ()
+-- main = do
+--   putStrLn "Hello! What's your name?"
+--   name <- getLine
+--   let statement = helloPerson name
+--   putStrLn statement
+
+firstWord :: String
+firstWord = "pessimism"
+
+secondWord :: T.Text
+secondWord = T.pack firstWord
+
+thirdWord :: String
+thirdWord = T.unpack secondWord
+
+myWord :: T.Text
+myWord = "dog"
+
+myNum1 :: Int
+myNum1 = 5
