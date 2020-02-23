2d81e3957530e13da0c062fa1a82f3e2e9b9d375
juryokun <capeta.kozo@gmail.com>
2020-02-24 00:40:56 +0900

    2020/02/23 rust

--- rust/bitonic-sorter/src/lib.rs
+++ rust/bitonic-sorter/src/lib.rs
@@ -1,9 +1,10 @@
 pub mod first;
 pub mod second;
+pub mod third;
 
 // SortOrderを列挙型として定義する
 pub enum SortOrder {
     // SortOrderには2つのバリアントがある
-    Ascending,     // 昇順
-    Descending,    // 降順
+    Ascending,  // 昇順
+    Descending, // 降順
 }
