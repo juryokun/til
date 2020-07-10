3e6c03e8368b78ca4acdea362c6366b84776ac28
juryokun <capeta.kozo@gmail.com>
2020-07-10 21:24:54 +0900

    2020/7/10 puzzle Q03 高階関数で別解

--- python/puzzle/q03.py
+++ python/puzzle/q03.py
@@ -17,4 +17,28 @@ def turn_over(card, number):
         card[1] = not card[1]
     return card
 
+
+
+def main_another():
+    MAX_CNT = 101
+    card_list = [[i, False] for i in range(1, MAX_CNT)]
+
+    for start in range(2, MAX_CNT, 1):
+        func = gen_turn_over_func(start)
+        map(func, card_list)
+
+    for card in filter(lambda x: x[1] == False, card_list):
+        print(card[0])
+
+
+def gen_turn_over_func(number):
+    def turn_over(card):
+        if card[0] % number == 0:
+            card[1] = not card[1]
+        return card
+    return turn_over
+
+
+
 main()
+main_another()
--- python/puzzle/test_q03.py
+++ python/puzzle/test_q03.py
@@ -3,6 +3,7 @@
 import unittest
 
 from q03 import turn_over
+from q03 import gen_turn_over_func
 
 class TestTurnOver(unittest.TestCase):
     def test_turn_over(self):
@@ -14,6 +15,20 @@ class TestTurnOver(unittest.TestCase):
         ]
         for target in data:
             result = turn_over(target[0], target[1])
+            if result != target[2]:
+                print(target[0])
+            self.assertEqual(result, target[2])
+
+    def test_gen_turn_over_func(self):
+        data = [
+            [[2, False], 2, [2, True]],
+            [[9, False], 3, [9, True]],
+            [[29, False], 5, [29, False]],
+            [[44, True], 4, [44, False]],
+        ]
+        for target in data:
+            func = gen_turn_over_func(target[1])
+            result = func(target[0])
             if result != target[2]:
                 print(target[0])
             self.assertEqual(result, target[2])
