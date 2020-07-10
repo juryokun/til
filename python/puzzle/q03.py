d53dc07d537a2962cdba3ad7d22e17e0ae482f86
juryokun <capeta.kozo@gmail.com>
2020-07-10 21:10:53 +0900

    2020/7/10 puzzle Q03 pythonで実装。mapでやってみた

--- python/puzzle/q03.py
+++ python/puzzle/q03.py
@@ -6,11 +6,15 @@ def main():
     card_list = [[i, False] for i in range(1, MAX_CNT)]
 
     for start in range(2, MAX_CNT, 1):
-        inc = start
-        for index in range(start, MAX_CNT, inc):
-            card_list[index-1][1] = not card_list[index-1][1]
+        vals = [start for i in range(len(card_list))]
+        map(turn_over, card_list, vals)
 
     for card in filter(lambda x: x[1] == False, card_list):
         print(card[0])
+
+def turn_over(card, number):
+    if card[0] % number == 0:
+        card[1] = not card[1]
+    return card
 
 main()
---
+++ python/puzzle/test_q03.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from q03 import turn_over
+
+class TestTurnOver(unittest.TestCase):
+    def test_turn_over(self):
+        data = [
+            [[2, False], 2, [2, True]],
+            [[9, False], 3, [9, True]],
+            [[29, False], 5, [29, False]],
+            [[44, True], 4, [44, False]],
+        ]
+        for target in data:
+            result = turn_over(target[0], target[1])
+            if result != target[2]:
+                print(target[0])
+            self.assertEqual(result, target[2])
+
+if __name__ == "__main__":
+    unittest.main()
