8ce29d82e20d7efda5b94651becb59a71e1fb7e0
juryokun <capeta.kozo@gmail.com>
2020-07-15 23:19:50 +0900

    2020/7/15 puzzle
    Q05 python 再帰で実装

--- python/puzzle/q05.py
+++ python/puzzle/q05.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import copy
+
 def main(num):
     cnt = 0
     for a in range(101):
@@ -13,6 +15,22 @@ def main(num):
                         print('a={a}, b={b}, c={c}, d={d}'.format(a=a,b=b,c=c,d=d))
     print(cnt)
 
+rel = 0
+def change(target, coins, limit, count=0):
+    if target == 0 and count <= limit and count > 0:
+        global rel
+        rel += 1
+        return 0
+    if target < 0:
+        return 0
+    if count > limit:
+        return 0
+    if len(coins) > 0:
+        coin = coins.pop()
+        for num in range(limit+1):
+            change(target-coin*num, copy.copy(coins), limit, count+num)
 
 if __name__ == "__main__":
     main(1000)
+    change(1000, [10, 50, 100, 500], 15)
+    print(rel)
