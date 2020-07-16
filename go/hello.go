d26b511baf7a02348e608084c90abc97da6e7eb4
juryokun <capeta.kozo@gmail.com>
2020-07-16 23:42:44 +0900

    2020/7/16 golang
    hello world.

--- go/hello.go
+++ go/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Print("火星の表面で、私の体重は")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print("ポンド、年齢は、")
+	fmt.Print(41 * 365 / 687)
+	fmt.Println("歳になるでしょう。")
 }
