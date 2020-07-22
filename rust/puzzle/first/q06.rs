dc2fbed987bb7c25ca23b2ed9727d4bb1039f0c3
juryokun <capeta.kozo@gmail.com>
2020-07-22 23:46:06 +0900

    2020/7/22 rust学習

--- rust/puzzle/first/q06.rs
+++ rust/puzzle/first/q06.rs
@@ -1,22 +1,18 @@
 fn main() {
     let mut cnt = 0;
-    for i in 0..10001 {
-        cnt += colatz(i, i, 1);
+    for i in 0..1001 {
+        cnt += colatz(i, i * 3 + 1);
     }
     println!("{}", cnt);
 }
 
-fn colatz(target: usize, n: usize, loop_num: usize) -> usize {
-    if n == 1 && loop_num != 1 {
+fn colatz(target: usize, mut n: usize) -> u8 {
+    n = if n % 2 == 0 { n / 2 } else { n * 3 + 1 };
+    if n == 1 {
         return 0;
-    }
-    if n == target && loop_num != 1 {
+    } else if n == target {
         return 1;
     }
 
-    if n % 2 == 0 && loop_num != 1 {
-        return colatz(target, n / 2, loop_num + 1);
-    } else {
-        return colatz(target, n * 3 + 1, loop_num + 1);
-    }
+    colatz(target, n)
 }
