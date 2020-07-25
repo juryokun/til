828a350ab8cfecb80ad6315a6dd5face5deb990b
juryokun <capeta.kozo@gmail.com>
2020-07-25 22:02:37 +0900

    2020/7/25 rust学習

--- rust/puzzle/first/q07.rs
+++ rust/puzzle/first/q07.rs
@@ -5,15 +5,19 @@ fn main() {
     let mut date = Utc.ymd(1964, 10, 10);
     let mut cnt = 0;
     while date != Utc.ymd(2020, 7, 24) {
-        let date_str = date.format("%Y%m%d").to_string().parse::<usize>().unwrap();
-        let date_2pre = format!("{:b}", date_str);
-        let date_2aft = date_2pre.chars().rev().collect::<String>();
-        let date_str_aft = usize::from_str_radix(&date_2aft, 2).unwrap();
-        if date_str == date_str_aft {
-            println!("{}", date_str);
+        let date_bin = ymd_to_str(date);
+        if date_bin == date_bin.chars().rev().collect::<String>() {
+            println!("{}", date);
             cnt += 1;
         }
         date = date + Duration::days(1);
     }
     println!("{}", cnt);
+}
+
+fn ymd_to_str(date: Date<Utc>) -> String {
+    format!(
+        "{:b}",
+        date.format("%Y%m%d").to_string().parse::<usize>().unwrap()
+    )
 }
