1489888a893ddfd52b9fec04a7a75cef9f404783
juryokun <capeta.kozo@gmail.com>
2020-08-23 00:20:09 +0900

    2020/8/23 rust学習

--- rust/puzzle/second/q13.rs
+++ rust/puzzle/second/q13.rs
@@ -1,8 +1,18 @@
+extern crate regex;
 use eval::{eval, to_value};
+use regex::Regex;
+use std::collections::HashSet;
 
 fn main() {
-    let target = "read+write+talk=skill";
+    let target = "READ+WRITE+TALK=SKILL";
     // 正規表現でアルファベットだけ抜き出す
+    let re = Regex::new(r"(\+|=|-)").unwrap();
+    let result = re.replace_all(target, "");
+    let mut uniq = HashSet::new();
+    for val in result.chars() {
+        uniq.insert(val);
+    }
+    println!("{:?}", uniq);
 
     // アルファベットの重複削除
 
