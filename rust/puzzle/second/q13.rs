1a5bc1086b66e4ae67244112c99f41ddf92265f4
juryokun <capeta.kozo@gmail.com>
2020-08-23 14:14:18 +0900

    2020/8/23 rust学習

--- rust/puzzle/second/q13.rs
+++ rust/puzzle/second/q13.rs
@@ -6,15 +6,21 @@ use std::collections::HashSet;
 fn main() {
     let target = "READ+WRITE+TALK=SKILL";
     // 正規表現でアルファベットだけ抜き出す
-    let re = Regex::new(r"(\+|=|-)").unwrap();
-    let result = re.replace_all(target, "");
+    let re = Regex::new(r"[^a-zA-Z]").unwrap();
+    let words = re.split(target).collect::<Vec<_>>();
+
+    // 頭文字を抜き出す
+    let heads = words
+        .iter()
+        .map(|w| w.chars().next().unwrap())
+        .collect::<Vec<_>>();
+
+    // アルファベットの重複削除
     let mut uniq = HashSet::new();
-    for val in result.chars() {
+    for val in words.join("").chars() {
         uniq.insert(val);
     }
     println!("{:?}", uniq);
-
-    // アルファベットの重複削除
 
     // 各アルファベットに数値割当（頭0省く）
 
