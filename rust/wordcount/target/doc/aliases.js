e748c8425a5e82741d0860c16f3c9926256cbde8
juryokun <capeta.kozo@gmail.com>
2020-08-31 20:08:28 +0900

    delete doc

--- rust/wordcount/target/doc/aliases.js
+++
@@ -1,8 +0,0 @@
-var ALIASES = {};
-ALIASES["aho_corasick"] = {};
-ALIASES["lazy_static"] = {};
-ALIASES["memchr"] = {};
-ALIASES["regex"] = {};
-ALIASES["regex_syntax"] = {};
-ALIASES["thread_local"] = {};
-ALIASES["wordcount"] = {};
