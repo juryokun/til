4795791ba3feb7e51ab4f4b8fb6670cb4580a68f
juryokun <capeta.kozo@gmail.com>
2020-09-21 12:01:30 +0900

    2020/9/21 rust学習
    puzzle 2-15

--- rust/puzzle/second/q15.rs
+++ rust/puzzle/second/q15.rs
@@ -1,21 +1,33 @@
+use std::collections::HashMap;
+
+const STAIR_MAX: i32 = 100;
+const STEP_MAX: i32 = 5;
+
 fn main() {
-    println!("{}", stair(0, 10));
+    let mut memo: HashMap<(i32, i32), usize> = HashMap::new();
+    println!("{}", stair(0, STAIR_MAX, &mut memo));
 }
 
-fn stair(a: i32, b: i32) -> usize {
-    if a > b || (a - b).abs() == 1 {
+fn stair(p: i32, q: i32, memo: &mut HashMap<(i32, i32), usize>) -> usize {
+    if let Some(&v) = memo.get(&(p, q)) {
+        return v;
+    }
+    if p > q || (p - q).abs() == 1 {
+        memo.insert((p, q), 0);
         return 0;
     }
-    if a == b {
+    if p == q {
+        memo.insert((p, q), 1);
         return 1;
     }
 
     let mut cnt: usize = 0;
-    for i in 1..=4 {
-        for j in 1..=4 {
-            // println!("{},{}", a, b);
-            cnt += stair(a + i, b - j);
+    for i in 1..=STEP_MAX {
+        for j in 1..=STEP_MAX {
+            // println!("{},{}", p, q);
+            cnt += stair(p + i, q - j, memo);
         }
     }
+    memo.insert((p, q), cnt);
     cnt
 }
