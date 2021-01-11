e524883fe39bd24efb4249facb32f074706616aa
juryokun <capeta.kozo@gmail.com>
2021-01-11 14:27:48 +0900

    2021/1/11

--- rust/desktop_app/pwm/src/main.rs
+++ rust/desktop_app/pwm/src/main.rs
@@ -1,11 +1,30 @@
 #![windows_subsystem = "windows"]
 use iced::{
-    button, executor, Align, Application, Button, Column, Command, Container, Element, Font,
-    HorizontalAlignment, Length, Row, Settings, Subscription, Text,
+    button, executor, scrollable, Align, Application, Button, Column, Command, Container, Element,
+    Font, HorizontalAlignment, Length, PaneGrid, Row, Scrollable, Settings, Subscription, Text,
 };
+use regex::Regex;
+use serde::Deserialize;
+use serde::Serialize;
+use std::fs::File;
+
+const FILE_NAME: &str = "serviceList.csv";
+
+fn load_data() -> Vec<Record> {
+    let file = DataFile::new();
+    let mut rdr = csv::Reader::from_reader(file.file_open().unwrap());
+
+    let mut rel: Vec<Record> = vec![];
+    for result in rdr.deserialize() {
+        let record: Record = result.unwrap();
+        rel.push(record);
+    }
+    rel
+}
+
 fn main() {
     let mut settings = Settings::default();
-    settings.window.size = (400u32, 120u32);
+    // settings.window.size = (400u32, 120u32);
     GUI::run(settings);
 }
 #[derive(Clone, Debug)]
@@ -15,6 +34,7 @@ pub enum Message {
 }
 struct GUI {
     mode: button::State,
+    scroll: scrollable::State,
 }
 
 impl Application for GUI {
@@ -25,6 +45,7 @@ impl Application for GUI {
         (
             GUI {
                 mode: button::State::new(),
+                scroll: scrollable::State::new(),
             },
             Command::none(),
         )
@@ -39,15 +60,73 @@ impl Application for GUI {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        let mut context = Column::new();
+        let mut context = Scrollable::new(&mut self.scroll);
 
-        for i in 0..5 {
-            let row = Row::new()
-                .push(Text::new("test1"))
-                .spacing(20)
-                .push(Text::new("test2"));
+        let data = load_data();
+        for rec in data.iter() {
+            let row = Row::with_children(vec![
+                Column::new()
+                    .width(Length::FillPortion(1))
+                    .push(Text::new(&rec.service))
+                    .into(),
+                Column::new()
+                    .width(Length::FillPortion(1))
+                    .push(Text::new(&rec.id))
+                    .into(),
+                Column::new()
+                    .width(Length::FillPortion(1))
+                    .push(Text::new(&rec.mail))
+                    .into(),
+                Column::new()
+                    .width(Length::FillPortion(1))
+                    .push(Text::new(&rec.password))
+                    .into(),
+                Column::new()
+                    .width(Length::FillPortion(1))
+                    .push(Text::new(&rec.memo))
+                    .into(),
+            ]);
             context = context.push(row);
         }
         Container::new(context).into()
     }
+}
+
+struct DataFile {
+    name: String,
+    home_path: String,
+    file_path: String,
+}
+impl DataFile {
+    fn new() -> Self {
+        let name = FILE_NAME.to_string();
+        let home_path = Self::get_home_path();
+        let file_path = format!("{}/{}", home_path, name);
+        Self {
+            name: name,
+            home_path: home_path,
+            file_path: file_path,
+        }
+    }
+    #[cfg(any(unix))]
+    fn get_home_path() -> String {
+        let home = std::env::var("HOME");
+        home.unwrap()
+    }
+    #[cfg(target_os = "windows")]
+    fn get_home_path() -> String {
+        let userprofile = std::env::var("USERPROFILE");
+        userprofile.unwrap()
+    }
+    fn file_open(&self) -> Result<File, std::io::Error> {
+        File::open(&self.file_path)
+    }
+}
+#[derive(Debug, Deserialize, Serialize)]
+struct Record {
+    service: String,
+    id: String,
+    mail: String,
+    password: String,
+    memo: String,
 }
