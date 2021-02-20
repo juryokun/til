32dd26dfd6a7b5a7f0625c5d8e621df1ea05b00e
juryokun <capeta.kozo@gmail.com>
2021-02-21 00:54:49 +0900

    2021/2/21

--- python/experiment/domain/01/inmemoryuserrepository.py
+++ python/experiment/domain/01/inmemoryuserrepository.py
@@ -7,16 +7,19 @@ class InMemoryUserRepository(IUserRepository):
 
     def __init__(self):
         test_user = User(UserName("yamada"))
-        self.store = {test_user.user_id: test_user}
+        self.store = {test_user.id: test_user}
 
     def save(self, user: User):
-        self.store[user.user_id] = user
+        self.store[user.id] = user
 
     def find(self, name: UserName) -> User:
         for i in self.store.values():
-            if i.user_name == name:
+            if i.name == name:
                 return i
         return None
+
+    def delete(self, user: User):
+        pass
 
     def exists(self, user: User) -> bool:
         return user in self.store.values()
