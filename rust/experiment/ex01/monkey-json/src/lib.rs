895c195c72ade3fec2e452d916d7056d22a7b749
juryokun <capeta.kozo@gmail.com>
2022-01-01 10:33:24 +0900

    2022/1/1

--- rust/experiment/ex01/monkey-json/src/lib.rs
+++ rust/experiment/ex01/monkey-json/src/lib.rs
@@ -1,1 +1,51 @@
 mod lexer;
+mod parser;
+
+use parser::*;
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    String(String),                  // 文字列
+    Number(f64),                     // 数値
+    Bool(bool),                      // 真偽値
+    Null,                            // Null
+    Array(Vec<Value>),               // JSON Array
+    Object(BTreeMap<String, Value>), // JSON Object
+}
+
+/// ユーザのエンドポイント
+/// 入力のJSON文字列から`Value`を返す
+pub fn parse(input: &str) -> Result<Value, ParserError> {
+    todo!()
+}
+
+/// {"key": true}
+/// v["key"] => Value::Bool(true)
+impl std::ops::Index<&str> for Value {
+    type Output = Value;
+    fn index(&self, key: &str) -> &Self::Output {
+        match self {
+            Value::Object(map) => map
+                .get(key)
+                .unwrap_or_else(|| panic!("A key is not found: {}", key)),
+            _ => {
+                panic!("A value is not object");
+            }
+        }
+    }
+}
+
+/// [null, false, 3]
+/// v[3] => Value::Number(3f64)
+impl std::ops::Index<usize> for Value {
+    type Output = Value;
+    fn index(&self, idx: usize) -> &Self::Output {
+        match self {
+            Value::Array(array) => &array[idx],
+            _ => {
+                panic!("A value is not array");
+            }
+        }
+    }
+}
---
+++ rust/experiment/ex01/monkey-json/src/parser.rs
@@ -0,0 +1,14 @@
+use crate::{lexer::Token, Value};
+
+#[derive(Debug, Clone)]
+pub struct ParserError {
+    pub msg: String,
+}
+
+impl ParserError {
+    pub fn new(msg: &str) -> ParserError {
+        ParserError {
+            msg: msg.to_string(),
+        }
+    }
+}
